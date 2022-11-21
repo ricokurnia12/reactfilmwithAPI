c5a3e470d212b7de018d6a626a1ec7f388545b84
bafir24 <bafirgenius@gmail.com>
2022-11-21 16:32:59 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [TopRateMovie, setTopRateMovie] = useState([]);
+
+    useEffect(() => {
+        getMovieList().then((result) => {
+            setTopRateMovie(result);
+        });
+    }, []);
+
+    const RatedMovieList = () => {
+        return TopRateMovie.map((movie, i) => {
+            return (
+                <div className="Movie-wrapper" key={i}>
+                    <div className="Movie-title">{movie.title}</div>
+                    <img
+                        className="Movie-image"
+                        src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+                    />
+
+                    <div className="Movie-date">
+                        {movie.release_date}
+                    </div>
+                    <div className="Movie-rate">
+                        {movie.vote_average}
+                    </div>
+                </div>
+            );
+        });
+    };
+    const search = async (q) => {
+        if (q.length >= 1) {
+            const query = await searchMovie(q);
+            setTopRateMovie(query.results);
+        }
+    };
+
+    return (
+        <div>
+            <header className="App-header">
+                <h1>Coco Movie</h1>
+                <input
+                    placeholder="search movies"
+                    className="Movie-search"
+                    onChange={({ target }) => search(target.value)}
+                />
+                <div className="Movie-container">
+                    <RatedMovieList />
+                </div>
+            </header>
+        </div>
+    );
+};
 
 export default App;
